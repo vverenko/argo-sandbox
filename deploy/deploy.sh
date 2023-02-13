53d1ac967c0952e2117b64305d7bac371764e9b2
Volodymyr Verenko <Verenko2000@gmail.com>
2023-02-13 13:33:54 +0200

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,26 +10,26 @@ COMMIT_MESSAGE="[BOT] update tags"
 
 if [ -n "${SSH_DEPLOY_KEY:=}" ]
 then
-	echo "[+] Using SSH_DEPLOY_KEY"
-
-	mkdir --parents "$HOME/.ssh"
-	DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
-	echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
-	chmod 600 "$DEPLOY_KEY_FILE"
-
-	SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
-	ssh-keyscan -H "$GITHUB_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
-
-	export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
-
-	GIT_CMD_REPOSITORY="git@$GITHUB_SERVER:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
+    echo "[+] Using SSH_DEPLOY_KEY"
+    
+    mkdir --parents "$HOME/.ssh"
+    DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
+    echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
+    chmod 600 "$DEPLOY_KEY_FILE"
+    
+    SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+    ssh-keyscan -H "$GITHUB_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
+    
+    export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+    
+    GIT_CMD_REPOSITORY="git@$GITHUB_SERVER:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
 else
-	echo "::error::API_TOKEN_GITHUB and SSH_DEPLOY_KEY are empty. Please fill one (recommended the SSH_DEPLOY_KEY)"
-	exit 1
+    echo "::error::API_TOKEN_GITHUB and SSH_DEPLOY_KEY are empty. Please fill one (recommended the SSH_DEPLOY_KEY)"
+    exit 1
 fi
 
 git config --global user.email "updater@email.com"
-git config --global user.name "[BOT] UPDATE TAGS"
+git config --global user.name "[BOT]"
 
 echo "[+] Git version"
 git --version
@@ -40,7 +40,15 @@ git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITO
 
 cd "$CLONE_DIR"
 
-touch test.txt && echo "test $RANDOM" > test.txt
+rm -rf temp.yml
+
+{
+    printf 'tags:\n'
+    printf '\tnginx: 1.19.0\n'
+	printf '\tnginx: 1.20.0\n'
+} </etc/hosts >yourfile.yaml
+
+
 git add test.txt
 git commit -m "$COMMIT_MESSAGE"
 git push origin "$TARGET_BRANCH"
